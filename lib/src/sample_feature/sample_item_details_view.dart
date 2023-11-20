bc31aeec45adb70a0490ec1578f2b212f1c6b342
Andrei Ciobanu <aciobanu@fitbit.com>
2023-11-20 18:00:56 +0200

    Handle link taps

--- lib/src/sample_feature/sample_item_details_view.dart
+++ lib/src/sample_feature/sample_item_details_view.dart
@@ -1,6 +1,7 @@
+import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter/material.dart';
 import 'package:recorder_rss_reader/src/sample_feature/sample_item.dart';
-import 'package:flutter_html/flutter_html.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 /// Displays detailed information about a SampleItem.
 class SampleItemDetailsView extends StatelessWidget {
@@ -15,13 +16,19 @@ class SampleItemDetailsView extends StatelessWidget {
       appBar: AppBar(
         title: Text(item.title),
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Html(
-            data: item.content,
-          ),
+      body: ListView(shrinkWrap: true, children: [
+        Html(
+          data: item.content,
+          onLinkTap: (url, attributes, element) async {
+            var uri = Uri.parse(url ?? "");
+            if (await canLaunchUrl(uri)) {
+              await launchUrl(uri);
+            } else {
+              throw Exception('Could not launch $uri');
+            }
+          },
         ),
-      ),
+      ]),
     );
   }
 }
